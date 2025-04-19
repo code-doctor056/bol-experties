21660009c058df1b6de6cdda6ae4bc2a413ac77c
expert056 <hiro.dev7777@gmail.com>
2025-04-19 14:22:29 -0600

    fix: increse animation time to 1500 ms

--- lib/features/auth/screen/ServiceSelectionScreen.dart
+++ lib/features/auth/screen/ServiceSelectionScreen.dart
@@ -8,7 +8,8 @@ class ServiceSelectionScreen extends StatefulWidget {
   State<ServiceSelectionScreen> createState() => _ServiceSelectionScreenState();
 }
 
-class _ServiceSelectionScreenState extends State<ServiceSelectionScreen> with SingleTickerProviderStateMixin{
+class _ServiceSelectionScreenState extends State<ServiceSelectionScreen>
+    with SingleTickerProviderStateMixin {
   int _selectedService = 0;
 
   late AnimationController _animationController;
@@ -18,33 +19,32 @@ class _ServiceSelectionScreenState extends State<ServiceSelectionScreen> with Si
     ServiceOption(
       title: "BPM report + tax return",
       subtitle:
-      "Get accurate tax estimates for importing vehicles based on current regulations",
+          "Get accurate tax estimates for importing vehicles based on current regulations",
       icon: Icons.receipt_long_outlined,
     ),
     ServiceOption(
       title: "Service 2",
       subtitle:
-      "Detailed assessment of vehicle damage with repair cost estimates",
+          "Detailed assessment of vehicle damage with repair cost estimates",
       icon: Icons.car_repair_outlined,
     ),
     ServiceOption(
       title: "Service 3",
-      subtitle:
-      "Current market value analysis based on real-time data",
+      subtitle: "Current market value analysis based on real-time data",
       icon: Icons.analytics_outlined,
     ),
   ];
 
   @override
-  void initState(){
+  void initState() {
     super.initState();
     _animationController = AnimationController(
-      vsync:this,
-      duration:const Duration(milliseconds:800)
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
     );
 
-    _progressAnimation =Tween <double>(begin:0.0, end:0.25,).animate(
-      CurvedAnimation(parent: _animationController, curve:Curves.easeInOut),
+    _progressAnimation = Tween<double>(begin: 0.0, end: 0.25).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
     );
 
     _animationController.forward();
@@ -75,24 +75,24 @@ class _ServiceSelectionScreenState extends State<ServiceSelectionScreen> with Si
                 ],
               ),
               const SizedBox(height: 6),
-              AnimatedBuilder(animation: _progressAnimation, builder:(context, child){
-                return LinearProgressIndicator(
-                  value: _progressAnimation.value,
-                  backgroundColor: Colors.grey.shade300,
-                  color:Colors.black,
-                  minHeight:4,
-                );
-              }),
+              AnimatedBuilder(
+                animation: _progressAnimation,
+                builder: (context, child) {
+                  return LinearProgressIndicator(
+                    value: _progressAnimation.value,
+                    backgroundColor: Colors.grey.shade300,
+                    color: Colors.black,
+                    minHeight: 4,
+                  );
+                },
+              ),
 
               const SizedBox(height: 24),
 
               /// Title
               const Text(
                 "Which service do you need?",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 6),
               const Text(
@@ -105,57 +105,62 @@ class _ServiceSelectionScreenState extends State<ServiceSelectionScreen> with Si
               ...List.generate(services.length, (index) {
                 final service = services[index];
                 return GestureDetector(
-                  onTap: (){
-                    setState((){
+                  onTap: () {
+                    setState(() {
                       _selectedService = index;
                     });
                   },
                   child: Container(
                     padding: const EdgeInsets.all(12),
-                    margin: const EdgeInsets.only(bottom:16),
+                    margin: const EdgeInsets.only(bottom: 16),
                     decoration: BoxDecoration(
                       border: Border.all(
-                        color: _selectedService == index ? Colors.black : Colors.grey.shade300,
+                        color:
+                            _selectedService == index
+                                ? Colors.black
+                                : Colors.grey.shade300,
                       ),
-                      borderRadius: BorderRadius.circular(12)
+                      borderRadius: BorderRadius.circular(12),
                     ),
-                    child:Row(
-                      children:[
-                        Icon(service.icon, size:28),
-                        const SizedBox(width:12),
+                    child: Row(
+                      children: [
+                        Icon(service.icon, size: 28),
+                        const SizedBox(width: 12),
                         Expanded(
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
-                            children:[
+                            children: [
                               Text(
                                 service.title,
                                 style: const TextStyle(
                                   fontWeight: FontWeight.w600,
-                                  fontSize:16,
-                                )
+                                  fontSize: 16,
+                                ),
                               ),
-                              const SizedBox(height:4),
-                              Text(service.subtitle,
-                              style: const TextStyle(
-                                fontSize:13,
-                                color:Colors.black54
-                              ),)
-                            ]
-                          )
+                              const SizedBox(height: 4),
+                              Text(
+                                service.subtitle,
+                                style: const TextStyle(
+                                  fontSize: 13,
+                                  color: Colors.black54,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
                         Radio<int>(
                           value: index,
                           groupValue: _selectedService,
                           onChanged: (value) {
-                            setState((){
-                              _selectedService= value!;
+                            setState(() {
+                              _selectedService = value!;
                             });
                           },
-                          activeColor:Colors.blue,
-                        )
-                      ]
-                    )
-                  )
+                          activeColor: Colors.blue,
+                        ),
+                      ],
+                    ),
+                  ),
                 );
               }),
 
@@ -168,7 +173,9 @@ class _ServiceSelectionScreenState extends State<ServiceSelectionScreen> with Si
                   onPressed: () {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder:(context)=> const ChooseTimeSlotScreen())
+                      MaterialPageRoute(
+                        builder: (context) => const ChooseTimeSlotScreen(),
+                      ),
                     );
                   },
                   style: ElevatedButton.styleFrom(
@@ -194,7 +201,6 @@ class ServiceOption {
   final IconData icon;
   final String title;
   final String subtitle;
-
 
   ServiceOption({
     required this.icon,
